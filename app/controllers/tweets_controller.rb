e6b537504fbd5299d0b20f8b8f1cc69bbac2b710
Hayashi-Satoki <stk.884@gmail.com>
2021-06-03 15:56:44 +0900

    move_to_indexの定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -38,5 +38,10 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 
 end
